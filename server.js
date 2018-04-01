30b3746310ab996dea016fc11080d29610d69fd2
singhVikram97 <vikram.bedi97@gmail.com>
2018-04-01 23:36:59 +0530

    Basic socket setup

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
 const express=require('express');
-const socket=require('socket.io');
+const socketio=require('socket.io');
 const path=require('path');
+const http=require('http');
 
 const app=express();
 
+
+// Need to run both app and socket.io on same server
+
+// Creates a server over which app runs
+const server=http.createServer(app);
+
+// Server over which socket.io app runs
+const io=socketio(server);
+
+// Client side library included at /socket.io/socket.io.js
+// io object like dollar of jquery available on console
 app.use('/',express.static(path.join(__dirname,'public')));
 
-app.listen(4444,function () {
+
+// Not app.listen
+server.listen(4444,function () {
    console.log("Server started at http://localhost:4444");
 });
