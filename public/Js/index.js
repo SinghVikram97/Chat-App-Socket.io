578a4d640d73284c381c69bab541cd8de9b43e45
singhVikram97 <vikram.bedi97@gmail.com>
2018-04-02 07:59:38 +0530

    Basic chat completed

--- public/Js/index.js
+++ public/Js/index.js
@@ -1,3 +1,29 @@
+let socket=io();
+
 $(document).ready(function () {
-   console.log("Hi");
+
+    let login=$("#login-container");
+    let chat=$("#chat-container");
+
+    let username=$("#username");
+    let loginBtn=$("#login");
+
+    let message=$("#message");
+    let send=$("#send");
+    let list=$("#list");
+
+    send.click(function () {
+        let msg=message.val();
+        let user=username.val();
+        message.val('');
+        username.val('');
+        socket.emit('sendMsg',{message:msg,username:user});
+    });
+
+    socket.on('recMsg',data=>{
+        console.log(data.message);
+        console.log(data.username);
+       list.append('<li>'+data.username+':'+data.message+'</li>')
+    })
+
 });
--- server.js
+++ server.js
@@ -18,6 +18,14 @@ const io=socketio(server);
 // io object like dollar of jquery available on console
 app.use('/',express.static(path.join(__dirname,'public')));
 
+io.on('connection',socket => {
+   console.log(socket.id);  // New Id on opening new tab or refresh page
+
+    socket.on('sendMsg',data=>{
+       // Json data received
+        io.emit('recMsg',{message:data.message,username:data.username});
+    })
+});
 
 // Not app.listens
 server.listen(4444,function () {
